160f09a469ac39e15047e49bcc6429025722a81c
Vladimir Nebolsin <taranis0123@gmail.com>
2023-01-20 22:51:03 +0400

    feat: add desktop version

--- appv2.js
+++ appv2.js
@@ -1,31 +1,54 @@
 const categoryContainer = document.querySelector('#category-container')
+const desktopCategoryContainer = document.querySelector('#desktop-category-container')
 const category = document.querySelectorAll('#cat')
 const quantityCounter = document.querySelectorAll('#quantity')
 const items = document.querySelectorAll('#item')
 const modal = document.querySelector('#modal')
 const backdrop = document.querySelector('#backdrop')
 
-categoryContainer.addEventListener('click', event => {
-    unsetButtons(categoryContainer)
-    event.target.classList.toggle('active')
-    scrollToActive(categoryContainer, event.target)
-    let scrollTarget = document.querySelector(`[name="${event.target.dataset.cat}"]`)
-    console.log(scrollTarget)
-    window.scrollBy({
-        top: scrollTarget.getBoundingClientRect().top - 90,
-        behavior: 'smooth'
-    })
-    // scrollTarget.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
-})
+setScroll(categoryContainer)
+setScroll(desktopCategoryContainer)
+
+// categoryContainer.addEventListener('click', event => {
+//     unsetButtons(categoryContainer)
+//     event.target.classList.toggle('active')
+//     scrollToActive(categoryContainer, event.target)
+//     let scrollTarget = document.querySelector(`[name="${event.target.dataset.cat}"]`)
+//     console.log(scrollTarget)
+//     window.scrollBy({
+//         top: scrollTarget.getBoundingClientRect().top - 90,
+//         behavior: 'smooth'
+//     })
+//     // scrollTarget.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
+// })
+
+// desktopCategoryContainer.addEventListener('click', event => {
+//     unsetButtons(desktopCategoryContainer)
+//     event.target.classList.toggle('active')
+//     scrollToActive(desktopCategoryContainer, event.target)
+//     let scrollTarget = document.querySelector(`[name="${event.target.dataset.cat}"]`)
+//     console.log(scrollTarget)
+//     window.scrollBy({
+//         top: scrollTarget.getBoundingClientRect().top - 90,
+//         behavior: 'smooth'
+//     })
+// })
 
 window.addEventListener('scroll', () => {
     category.forEach(cat => {
         let rect = cat.getBoundingClientRect()
-        let isVisible = (rect.top >= -150 && rect.bottom <= window.innerHeight + 150)
+        console.log(rect, cat)
+        let isVisible = (rect.top >= -550 && rect.bottom <= window.innerHeight + 550)
+        console.log(isVisible)
+        console.log(window.innerHeight)
         if (isVisible) {
             let button = unsetButtons(categoryContainer)[cat.dataset.num - 1]
+            let buttonDesc = unsetButtons(desktopCategoryContainer)[cat.dataset.num - 1]
+            console.log(`До ${cat.dataset.num - 1}`)
             button.classList.toggle('active')
+            buttonDesc.classList.toggle('active')
             scrollToActive(categoryContainer, button)
+            // scrollToActive(categoryContainer, buttonDesc)
         }
     })
 })
@@ -71,4 +94,18 @@ function scrollToActive(buttonContainer, activeButton) {
         left: buttonContainer.scrollLeft + navItemsLeft,
         behavior: 'smooth'
     });
+}
+
+function setScroll(buttonContainer) {
+    buttonContainer.addEventListener('click', event => {
+        unsetButtons(buttonContainer)
+        event.target.classList.toggle('active')
+        scrollToActive(buttonContainer, event.target)
+        let scrollTarget = document.querySelector(`[name="${event.target.dataset.cat}"]`)
+        console.log(scrollTarget)
+        window.scrollBy({
+            top: scrollTarget.getBoundingClientRect().top - 90,
+            behavior: 'smooth'
+        })
+    })
 }
